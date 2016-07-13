66c57e08b10fc5700fd9b2387f0bb175de3c2707
Jeffery Bennett <meandmybadself@gmail.com>
2016-07-13 15:17:35 -0500

    Title-casing city in response object.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs-promise')
 const path = require('path')
+const toTitleCase = require('to-title-case');
 
 class Ziptastic {
   getInfo (code, country) {
@@ -18,7 +19,9 @@ class Ziptastic {
       try {
         let data = require(path.join(__dirname , 'data/ziptastic.' + country + '.json'))
         if (data[code]) {
-          return resolve(data[code])
+          let obj = data[code]
+          obj.city = toTitleCase(obj.city)
+          return resolve(obj)
         }
         return reject('Could not find specified postal code: ' + code)
       } catch(e) {
